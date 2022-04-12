7a248a32414c933a42ed14296e81b3da6754ae13
Yu Wang <yuwan@microsoft.com>
2022-04-11 21:06:55 -0700

    feat: add Basic Auth

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+# Usage
+
+![](docs/images/README/2022-04-11-21-05-04.png)
+
+Fill text in the text area and click "Submit".  In another device, refresh the web page to access.
+
+# Deploy to Azure
+
+Follow this [link](https://github.com/smartninja/example-azure-flask) to create an Azure App Service and connect to this GitHub Repository.
+
+Update the `startup command` to `gunicorn --bind=0.0.0.0 --timeout 600 cross_device_share.app:app`
+
+Add an environment variable called "USER" and "PASS", fill your selected user name and password.
 # Setup Dev Environment
 
 First clone this repo then change to the repo directory.
@@ -14,3 +27,4 @@ If you want to run unit test manually, just activate virtual environment and run
 ```sh
 pytest
 ```
+
--- cross_device_share/app.py
+++ cross_device_share/app.py
@@ -1,24 +1,40 @@
+import os
 from typing import List
 
 from flask import Flask, jsonify, redirect, request
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 cache: List[str] = list()
 MAX_SIZE = 10
 
+USER = os.environ["USER"]
+PASS = generate_password_hash(os.environ["PASS"])
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username == USER and check_password_hash(PASS, password):
+        return username
+
 
 @app.route("/")
+@auth.login_required
 def index():
     return app.send_static_file("index.html")
 
 
 @app.route("/get")
+@auth.login_required
 def get():
     return jsonify(cache)
 
 
 @app.route("/post_new", methods=["POST"])
+@auth.login_required
 def post_new():
     global cache
     cache.insert(0, request.form["content"])
